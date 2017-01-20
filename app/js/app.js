28236edfb7be88abbe69d25abff30b869b3e9199
Greg Pfaff <greg.pfaff@razorfish.com>
2017-01-20 17:44:15 -0600

    setting up dev enviromenment

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chart from './components/Charts';
+
+ReactDOM.render(
+  <Chart />, document.getElementById('app') // eslint-disable-line
+);
---
+++ app/js/components/Charts.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Chart extends Component {
+  render() {
+    return (
+      <div className="chart">
+        I need my charts JACK!
+
+        Hold on to your tits and bits
+      </div>
+    );
+  }
+}
+
+export default Chart;
