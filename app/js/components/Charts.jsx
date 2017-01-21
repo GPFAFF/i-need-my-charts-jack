41d0bc7dc4991bd09450744637080840ca041385
Greg Pfaff <greg.pfaff@razorfish.com>
2017-01-21 11:02:42 -0600

    adding working styles

--- app/js/components/Charts.jsx
+++ app/js/components/Charts.jsx
@@ -2,30 +2,26 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ChartistGraph from 'react-chartist';
 
+
 class Chart extends Component {
   render() {
-    var biPolarLineChartData = {
-      labels: [1, 2, 3, 4, 5, 6, 7, 8],
+    var biPolarBarChartData = {
+      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
       series: [
-        [1, 2, 3, 1, -2, 0, 1, 0],
-        [-2, -1, -2, -1, -2.5, -1, -2, -1],
-        [0, 0, 0, 1, 2, 2.5, 2, 1],
-        [2.5, 2, 1, 0.5, 1, 0.5, -1, -2.5]
+        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
       ]
-    }
-    var biPolarLineChartOptions = {
-      high: 3,
-      low: -3,
-      showArea: true,
-      showLine: false,
-      showPoint: false,
+    };
+    var biPolarBarChartOptions = {
+      high: 10,
+      low: -10,
       axisX: {
-        showLabel: false,
-        showGrid: false
+        labelInterpolationFnc: function(value, index) {
+          return index % 2 === 0 ? value : null;
+        }
       }
     }
     return (
-      <ChartistGraph data={biPolarLineChartData} options={biPolarLineChartOptions} type={'Line'} />
+      <ChartistGraph data={biPolarBarChartData} options={biPolarBarChartOptions} type={'Line'} />
     )
   }
 }
