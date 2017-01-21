3eb8f2df3d9d1377a95bf14d653467f55e7aa356
Greg Pfaff <greg.pfaff@razorfish.com>
2017-01-21 09:48:46 -0600

    adding working chart

--- app/js/app.js
+++ app/js/app.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import Chart from './components/Charts';
 
 ReactDOM.render(
-  <Chart />, document.getElementById('app') // eslint-disable-line
+  <Chart />,
+   document.getElementById('app') // eslint-disable-line
 );
