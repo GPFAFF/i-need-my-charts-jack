0f84e816c9cc490dceadbeb507f8c6749698e438
Greg Pfaff <greg.pfaff@razorfish.com>
2017-01-23 11:41:19 -0600

    adding files

--- app/js/components/Charts.jsx
+++ app/js/components/Charts.jsx
@@ -22,6 +22,7 @@ class Chart extends Component {
         this.setState({ chartData });
       });
   }
+  
   render() {
     var biPolarBarChartData = {
       labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
